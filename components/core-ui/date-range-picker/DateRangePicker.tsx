1ac7bc1e69012106da3bc4a9da9d5ca32e8f5ffa
Eleftherios Psitopoulos <lefpsi@gmail.com>
2023-03-24 18:06:46 +0100

    Fix DateRange bug that prevented showing the correct dates

--- components/core-ui/date-range-picker/DateRangePicker.tsx
+++ components/core-ui/date-range-picker/DateRangePicker.tsx
@@ -1,44 +1,15 @@
-import { useEffect, useState } from "react";
+import { DateHelpers } from "@crowdmetrix/date";
+import { useState } from "react";
 import { RangePicker } from "react-minimal-datetime-range";
 import "react-minimal-datetime-range/lib/react-minimal-datetime-range.min.css";
 
-const now = new Date();
-
 type Props = {
-  defaultStartEndDateTimes: [Date, Date];
   onConfirm: (res: [Date, Date]) => void;
-  placeholder?: [string, string];
-  disabled?: boolean;
 };
 
-const DateRangePicker = ({
-  defaultStartEndDateTimes,
-  onConfirm,
-  placeholder = ["Start Time", "End Time"],
-  disabled,
-}: Props) => {
-  // Set startDateTime and endDateTime to now initially, to enforce type to Date
-  // instead of making setting the type to Nullable<Date>
-  // The real values will be set below on the effect
-  const [startDateTime, setStartDateTime] = useState(now);
-  const [endDateTime, setEndDateTime] = useState(now);
-
-  useEffect(() => {
-    if (!startDateTime || !endDateTime) {
-      // No matter how the user passed the two dates, we make sure that the min of the
-      // two is set to startDateTime and the max of the two is set too endDateTime
-      setStartDateTime(
-        defaultStartEndDateTimes[0] < defaultStartEndDateTimes[1]
-          ? defaultStartEndDateTimes[0]
-          : defaultStartEndDateTimes[1]
-      );
-      setEndDateTime(
-        defaultStartEndDateTimes[0] < defaultStartEndDateTimes[1]
-          ? defaultStartEndDateTimes[1]
-          : defaultStartEndDateTimes[0]
-      );
-    }
-  }, []);
+const DateRangePicker = ({ onConfirm }: Props) => {
+  const [startDateTime, setStartDateTime] = useState(new Date());
+  const [endDateTime, setEndDateTime] = useState(new Date());
 
   /**
    * The RangePicker plugin returns an array of strings instead of Dates
@@ -52,35 +23,27 @@ const DateRangePicker = ({
   const onConfirmResponseTransformer = ([
     startDateResult,
     endDateResult,
-  ]: string[]) =>
-    onConfirm([new Date(startDateResult), new Date(endDateResult)]);
+  ]: string[]) => {
+    let startResultToDate = new Date(startDateResult);
+    let endResultToDate = new Date(endDateResult);
 
-  /**
-   * This normalises the passed Date object to the required format of the RangePicker component:
-   * * __defaultDate__: YYYY-MM-DD
-   * * __defaultTime__: hh:mm
-   *
-   * @param date
-   * @returns [YYYY-MM-DD, hh:mm]
-   */
-  const normalizeDateTime = (date: Date) => {
-    const year = date.getFullYear().toString();
-    const month = (date.getMonth() + 1).toString().padStart(2, "0");
-    const day = date.getDate().toString().padStart(2, "0");
-    const hour = date.getHours().toString().padStart(2, "0");
-    const minute = date.getMinutes().toString().padStart(2, "0");
-    return [`${year}-${month}-${day}`, `${hour}:${minute}`];
+    setStartDateTime(startResultToDate);
+    setEndDateTime(endResultToDate);
+    onConfirm([startResultToDate, endResultToDate]);
   };
 
-  const [startDate, startTime] = normalizeDateTime(startDateTime);
-  const [endDate, endTime] = normalizeDateTime(endDateTime);
+  // This normalises the passed Date object to the required format of the RangePicker component:
+  // * __defaultDate__: YYYY-MM-DD
+  // * __defaultTime__: hh:mm
+  // toDateAndTimeStirngFormat() returns [YYYY-MM-DD, hh:mm]
+  const [startDate, startTime] =
+    DateHelpers.toDateAndTimeStirngFormat(startDateTime);
+  const [endDate, endTime] = DateHelpers.toDateAndTimeStirngFormat(endDateTime);
 
   return (
     <RangePicker
       defaultDates={[startDate, endDate]}
       defaultTimes={[startTime, endTime]}
-      placeholder={placeholder}
-      disabled={disabled}
       onConfirm={onConfirmResponseTransformer}
     />
   );
